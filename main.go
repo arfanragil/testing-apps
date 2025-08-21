680ddc3d2204b21d0bd2d6482cf690b893285b0c
arfanragil <rraaggiill@gmail.com>
2025-08-21 12:03:09 +0700

    push update

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 		if name == "" {
 			name = "echo"
 		}
-		return c.String(http.StatusOK, "Hello, "+name+"! on v0.2")
+		return c.String(http.StatusOK, "Selamat Datang di sini yaa, "+name+"! on v0.2")
 	})
 
 	e.GET("/v1", func(c echo.Context) error {
